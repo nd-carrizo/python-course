c66c75b903c1b70181f549943a42a14f67063a49
Nestor Carrizo <ndc.nestor@gmail.com>
2026-09-22 15:36:30 +0000

    Week 1 - Exercise 2

---
+++ week-1/exercise2.py
@@ -0,0 +1,16 @@
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("zahl:", zahl)
+print("Type:", type(zahl))
+
+print("kommazahl:", kommazahl)
+print("Type:", type(kommazahl))
+
+print("text:", text)
+print("Type:", type(text))
+
+print("wahrheitswert:", wahrheitswert)
+print("Type:", type(wahrheitswert))

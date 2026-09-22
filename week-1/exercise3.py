eb4eb77e450e507029495cefee83c7cf127454d4
Nestor Carrizo <ndc.nestor@gmail.com>
2026-09-22 15:47:40 +0000

    Week 1 - Exercise 3

---
+++ week-1/exercise3.py
@@ -0,0 +1,18 @@
+integer = 9
+floating_point = 11.5
+number_string = "17"
+
+result1 = float(integer)
+print(result1)
+
+result2 = int(floating_point)
+print(result2)
+
+result3 = str(integer)
+print(result3)
+
+result4 = int(number_string)
+print(result4)
+
+result5 = bool(integer)
+print(result5)

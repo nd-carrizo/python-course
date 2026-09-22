86a99899870f3a10dfa575e778493a3ecac03c3d
Nestor Carrizo <ndc.nestor@gmail.com>
2026-09-22 14:52:49 +0000

    Week 1 - Exercise 1

---
+++ week-1/main.py
@@ -0,0 +1,5 @@
+from math import pi
+
+if __name__ == "__main__":
+    r = float(input("Input the radius of the circle: "))
+    print("The area of the circle with radius " + str(r) + " is: " + str(pi * r ** 2))
